789dc3a378565dbd958a2a04ae5ce5e3b10179e7
s2a0v2a4 <xcrush389@gmail.com>
2025-09-02 13:17:36 +0200

    pieces nf

---
+++ src/app/chess-logical/models.ts
@@ -0,0 +1,24 @@
+export enum Color{
+    White,
+    Black
+}
+
+export type Coords = {
+    x: number;
+    y: number;
+}
+
+export enum FENChar {
+   WhitePawn = 'P',
+   WhiteRook = 'R',
+   WhiteKnight = 'N',
+   WhiteBishop = 'B',
+   WhiteQueen = 'Q',
+   WhiteKing = 'K',
+   BlackPawn = 'p',
+   BlackRook = 'r',
+   BlackKnight = 'n',
+   BlackBishop = 'b',
+   BlackQueen = 'q',
+   BlackKing = 'k'
+}
