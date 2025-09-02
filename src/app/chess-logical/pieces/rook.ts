c91ddb2f328d72793586e8d110dca29119130957
s2a0v2a4 <xcrush389@gmail.com>
2025-09-02 22:44:20 +0200

    rook

---
+++ src/app/chess-logical/pieces/rook.ts
@@ -0,0 +1,26 @@
+import { Piece } from "./piece";
+
+export class Rook extends Piece{
+    private _hasMoved: boolean = false;
+    protected override _FENChar: FENChar = 'R';
+    protected override _directions: Coords[] = [
+        { x: 1, y: 0 },
+        { x: 0, y: 1 },
+        { x: -1, y: 0 },
+        { x: 0, y: -1 }
+    ];
+
+    constructor(private pieceColor: Color) {
+        super(pieceColor);
+        this._FENChar = pieceColor === Color.White ? FENChar.WhiteRook : FENChar.BlackRook;
+    }
+
+        public get hasMoved(): boolean {
+            return this._hasMoved;
+        }
+
+        public set hasMoved(value: boolean) {
+            this._hasMoved = value;
+        }
+
+}
